d537576cedcec06fda79b4e4ae580d47e3172272
Circuit Specialists <github@circuitspecialists.com>
2019-11-05 15:27:08 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Smart_Outlet
-Create your own outlet with an esp8266 for your smart home contorl. https://www.circuitspecialsts.com
+
+
+## Connect WiFi
+To connect with WiFi, create a txt file named, creds.txt. Put the SSID on one line, and the password on another. Save, push to esp, and reboot
+
+ex:
+something_SSID
+something_password
--- main.py
+++ main.py
@@ -1,42 +1,79 @@
 import network
 import esp
 
+
 class WiFi:
     def __init__(self, ssid, password):
-        wlan = network.WLAN(network.STA_IF) # create station interface
-        wlan.active(True)       # activate the interface
-        wlan.scan()             # scan for access points
-        wlan.isconnected()      # check if the station is connected to an AP
-        wlan.connect(ssid, password) # connect to an AP
-        wlan.config('mac')      # get the interface's MAC adddress
-        wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
+        self.wlan = network.WLAN(network.STA_IF)  # create station interface
+        self.wlan.active(True)       # activate the interface
+        self.wlan.scan()             # scan for access points
+        self.wlan.isconnected()      # check if the station is connected to an AP
+        self.wlan.connect(ssid, password)  # connect to an AP
+        self.wlan.config('mac')      # get the interface's MAC adddress
+        self.wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
 
-        ap = network.WLAN(network.AP_IF) # create access-point interface
-        ap.config(essid='ESP-AP') # set the ESSID of the access point
-        ap.active(True)         # activate the interface
+        self.ap = network.WLAN(network.AP_IF)  # create access-point interface
+        self.ap.config(essid='ESP-AP')  # set the ESSID of the access point
+        self.ap.active(True)         # activate the interface
+
 
 class NTP:
     def __init__(self):
-        rtc = RTC()
-        rtc.datetime((2017, 8, 23, 1, 12, 48, 0, 0)) # set a specific date and time
-        rtc.datetime() # get date and time
+        self.rtc = RTC()
+        # set a specific date and time
+        self.rtc.datetime((2017, 8, 23, 1, 12, 48, 0, 0))
+        self.rtc.datetime()  # get date and time
 
         # synchronize with ntp
         # need to be connected to wifi
         import ntptime
-        ntptime.settime() # set the rtc datetime from the remote server
-        rtc.datetime()    # get the date and time in UTC
+        ntptime.settime()  # set the rtc datetime from the remote server
+        self.rtc.datetime()    # get the date and time in UTC
+
+
+class CREDS:
+    def __init__(self):
+        import uos
+        files = uos.listdir()
+        if("creds.txt" in files):
+            self.getCreds()
+        else:
+            ssid = input("Enter SSID\t")
+            password = input("Enter password\t")
+            self.createCreds(ssid, password)
+
+    def createCreds(self, ssid, password):
+        f = open("creds.txt", "w")
+        f.write(str(ssid) + "\n")
+        f.write(str(password))
+        f.close()
+        self.SSID = ssid
+        self.password = password
+
+    def getCreds(self):
+        f = open("creds.txt", "r")
+        self.SSID = str(f.readline()).strip()
+        self.password = str(f.readline()).strip()
+        f.close()
+
 
 if __name__ == "__main__":
-    main = WiFi('circuitspecialists.com', 'C!rCu!t!')
     print(esp.check_fw())
+
     try:
-        from machine import RTC
-        NTP()
+        creds_handler = CREDS()
+        wifi = WiFi(creds_handler.SSID, creds_handler.password)
     except Exception as e:
         print(e)
+
+    try:
+        from machine import RTC
+        rtc = NTP()
+    except Exception as e:
+        print(e)
+
     try:
         import smart_control
-        main = smart_control.CONTROLLER()
+        controller = smart_control.CONTROLLER()
     except Exception as e:
         print(e)
