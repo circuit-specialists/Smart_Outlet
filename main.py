57b841a0880f2ff08608f8413a1f0d7d5a15768a
Mitch <Mitch@Ordertech.com>
2019-12-04 19:56:36 -0700

    Setup backend for time scheduling

--- main.py
+++ main.py
@@ -4,9 +4,14 @@
 """
 
 import network
+import ntptime
+import utime
+import time
+import machine
 import esp
-
-norepl = False
+import gc
+from sys import platform
+gc.collect()
 
 class WiFi:
     def __init__(self, ssid=None, password=None, type='AP'):
@@ -25,19 +30,82 @@ class WiFi:
             self.ap.config(password="circuit1234")
             self.ap.active(True)                        # activate the interface
 
+class WDT: #Software WatchDog system to reset the board if it shuts off unexpectedly
+    def __init__(self, id=0, timeout=120, use_rtc_memory=True): #Thank you kevinkk525 on the MicroPython Forums
+        self._timeout = timeout / 10
+        self._counter = 0
+        self._timer = machine.Timer(id)
+        self._use_rtc_memory = use_rtc_memory
+        self.init()
+        try:
+            with open("watchdog.txt", "r") as f:
+                if f.read() == "True":
+                    print("Reset reason: Watchdog")
+        except Exception as e:
+            print(e)  # file probably just does not exist
+        try:
+            with open("watchdog.txt", "w") as f:
+                f.write("False")
+        except Exception as e:
+            print("Error saving to file: {!s}".format(e))
+            if use_rtc_memory and platform == "esp8266":
+                rtc = machine.RTC()
+                if rtc.memory() == b"WDT reset":
+                    print("Reset reason: Watchdog")
+                rtc.memory(b"")
 
-class NTP:
-    def __init__(self):
-        self.rtc = RTC()
-        # set a specific date and time
-        self.rtc.datetime((2019, 8, 23, 1, 12, 48, 0, 0))
-        self.rtc.datetime()  # get date and time
+    def _wdt(self, t):
+        self._counter += self._timeout
+        if self._counter >= self._timeout * 10:
+            try:
+                with open("watchdog.txt", "w") as f:
+                    f.write("True")
+            except Exception as e:
+                print("Error saving to file: {!s}".format(e))
+                if self._use_rtc_memory and platform == "esp8266":
+                    rtc = machine.RTC()
+                    rtc.memory(b"WDT reset")
+            machine.reset()
 
-        # synchronize with ntp
-        # need to be connected to wifi
-        import ntptime
-        ntptime.settime()  # set the rtc datetime from the remote server
-        self.rtc.datetime()    # get the date and time in UTC
+    def feed(self):
+        self._counter = 0
+
+    def init(self, timeout=None):
+        timeout = timeout or self._timeout
+        self._timeout = timeout
+        self._timer.init(period=int(self._timeout * 1000), mode=machine.Timer.PERIODIC, callback=self._wdt)
+
+    def deinit(self):  # will not stop coroutine
+        self._timer.deinit()
+
+class TIME:
+    isupdated = False
+    def logToFile(status, time):
+        with open('log.txt','w') as file:
+            file.write(status)
+            file.write(str(time))
+        file.close()
+    def updateTime(self):
+        if utime.localtime()[4]%15==0 and not is_updated: #Checks if its been 15 minutes
+            try:
+                ntptime.settime()
+            except Exception as e:
+                print(e)
+            isupdated = True
+            time.sleep(0.5)
+        elif utime.localtime()[3]%15 != 0 and is_updated: #If it hasn't been 15 mins, set to not updated
+            isupdated = False
+    def checkTime(self):
+        if t[3]>=23 or t[3]<7 and led.value() == OFF:
+            write_to_file("ON", utime.localtime())
+            led.off()
+            print("ON")
+            utime.localtime()
+        elif t[3]>=7 and t[3]<23 and led.value() == ON:
+            led.on()
+            print("OFF")
+            utime.localtime()
+            write_to_file("OFF", utime.localtime())
 
 
 class CREDS:
@@ -69,8 +137,8 @@ if __name__ == "__main__":
         print("Creating Access Point...")
 
     try:
-        from machine import RTC
-        rtc = NTP()
+        timeSchedule = TIME()
+        watchDog = WDT()
     except Exception as e:
         print(e)
 
@@ -84,9 +152,8 @@ if __name__ == "__main__":
         import nanoWebSrv
         httpd = nanoWebSrv.NANOWEBSRV()
         while True:
-            try:
-                httpd.socketListener()
-            except Exception as e:
-                print(e)
+            httpd.socketListener()
+            timeSchedule.checkTime()
+            watchDog.feed()
     except Exception as e:
         print(e)
